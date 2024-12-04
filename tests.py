e435d9cebb2050a1a006416434885f851745d1ce
Arshad Fasiludeen <137993931+afasil9@users.noreply.github.com>
2024-12-04 17:07:22 +0000

    Added gmsh tests

--- tests.py
+++ tests.py
@@ -1,88 +1,139 @@
 #%%
 from mpi4py import MPI
-from dolfinx.fem import Constant
-from ufl import SpatialCoordinate, variable, as_vector, grad, curl, div, diff
-from utils import L2_norm, create_mesh_fenics
+from ufl import SpatialCoordinate, variable, as_vector, grad, curl, div, diff, sin, cos, pi
 from solver_non_sym import solver
-import ufl
 from dolfinx import fem
+from utils import L2_norm, create_mesh_fenics, create_mesh_gmsh
 
-comm = MPI.COMM_WORLD
-degree = 1
-n = 8
+# problem = "Fenics Mesh"
+problem = "Gmsh Mesh"
 
-ti = 0.0  # Start time
-T = 0.1  # End time
-num_steps = 100  # Number of time steps
-d_t = (T - ti) / num_steps  # Time step size
+if problem == "Fenics Mesh":
 
-boundaries = {"bottom": 1, "top": 2, "front": 3, "right": 4, "back": 5, "left": 6}
-domain,ft, ct = create_mesh_fenics(comm, n, boundaries)
+    print("Fenics Mesh")
+    comm = MPI.COMM_WORLD
+    degree = 1
+    n = 4
 
-x = SpatialCoordinate(domain)
-t = variable(fem.Constant(domain, ti))
-nu = 1.0
-sigma = 1.0
+    ti = 0.0  # Start time
+    T = 0.1  # End time
+    num_steps = 100  # Number of time steps
+    d_t = (T - ti) / num_steps  # Time step size
 
-def exact(x, t):
-    return as_vector((
-        x[1]**2 + x[0] * t, 
-        x[2]**2 + x[1] * t, 
-        x[0]**2 + x[2] * t))
+    boundaries = {"bottom": 1, "top": 2, "front": 3, "right": 4, "back": 5, "left": 6}
+    domain,ft, ct = create_mesh_fenics(comm, n, boundaries)
 
-def exact1(x):
-    return (x[0]**2) + (x[1]**2) + (x[2]**2)
+    x = SpatialCoordinate(domain)
+    t = variable(fem.Constant(domain, ti))
+    nu = 1.0
+    sigma = 1.0
 
-uex = exact(x,t)
-uex1 = exact1(x)
+    def exact(x, t):
+        return as_vector((cos(pi * x[1]) * sin(pi * t), cos(pi * x[2]) * sin(pi * t), cos(pi * x[0]) * sin(pi * t)))
 
-#Manually calculating the RHS
+    def exact1(x):
+        return sin(pi*x[0]) * sin(pi*x[1]) * sin(pi*x[2])
 
-# f0 = ufl.as_vector((
-#     -2 + 3*x[0],
-#     -2 + 3*x[1],
-#     -2 + 3*x[2])
-# )
+    uex = exact(x,t)
+    uex1 = exact1(x)
 
-# f1 = fem.Constant(domain, -9.0)
+    f0 = nu*curl(curl(uex)) + sigma*diff(uex,t) + sigma*grad(uex1)
+    f1 = -div(sigma*grad(uex1)) - div(sigma*diff(uex,t))
 
-f0 = nu*curl(curl(uex)) + sigma*diff(uex,t) + sigma*grad(uex1)
-f1 = -div(sigma*grad(uex1)) - div(sigma*diff(uex,t))
-
-bc_dict = {
-    "V": {
-        # Interpolate vector-valued exact solution on boundaries
-        (boundaries["bottom"], boundaries["top"], 
-        boundaries["front"], boundaries["right"],
-        boundaries["back"], boundaries["left"]): uex
-    },
-    "V1": {
-        # Interpolate scalar exact solution on all boundaries
-        (boundaries["bottom"], boundaries["top"], 
-        boundaries["front"], boundaries["right"],
-        boundaries["back"], boundaries["left"]): uex1
+    bc_dict = {
+        "V": {
+            # Interpolate vector-valued exact solution on boundaries
+            (boundaries["bottom"], boundaries["top"], 
+            boundaries["front"], boundaries["right"],
+            boundaries["back"], boundaries["left"]): uex
+        },
+        "V1": {
+            # Interpolate scalar exact solution on all boundaries
+            (boundaries["bottom"], boundaries["top"], 
+            boundaries["front"], boundaries["right"],
+            boundaries["back"], boundaries["left"]): uex1
+        }
     }
-}
 
-# preconditioner = 'AMS'
-preconditioner = 'AMS'
+    preconditioner = 'AMS'
 
-domain_tags = ct
+    domain_tags = ct
+
+    E,B = solver(comm, domain, ft, ct, degree, nu, sigma, t, d_t, num_steps, f0, f1, bc_dict, uex, uex1, preconditioner)
+
+    t_prev = T - d_t
+
+    uex_prev = exact(x, t_prev)
+    uex_final = exact(x, T)
+
+    da_dt_exact = (uex_final - uex_prev) / d_t
+    E_exact = -grad(uex1) - da_dt_exact
+
+    print("E field error", L2_norm(E - E_exact))
+    print("B field error", L2_norm(B - curl(uex)))
+
+elif problem == "Gmsh Mesh":
+
+    print("Gmsh Mesh")
+
+    comm = MPI.COMM_WORLD
+    degree = 1
+    n = 8
+
+    ti = 0.0  # Start time
+    T = 0.1  # End time
+    num_steps = 100  # Number of time steps
+    d_t = (T - ti) / num_steps  # Time step size
+
+    boundaries = {"bottom": 1, "top": 2, "front": 3, "right": 4, "back": 5, "left": 6}
+    domain, ft, ct = create_mesh_gmsh(MPI.COMM_WORLD, n, boundaries)
+
+    x = SpatialCoordinate(domain)
+    t = variable(fem.Constant(domain, ti))
+    nu = 1.0
+    sigma = 1.0
+
+    def exact(x, t):
+        return as_vector((cos(pi * x[1]) * sin(pi * t), cos(pi * x[2]) * sin(pi * t), cos(pi * x[0]) * sin(pi * t)))
+
+    def exact1(x):
+        return sin(pi*x[0]) * sin(pi*x[1]) * sin(pi*x[2])
+
+    uex = exact(x,t)
+    uex1 = exact1(x)
 
 
-E,B = solver(comm, domain, ft, ct, degree, nu, sigma, t, d_t, num_steps, f0, f1, bc_dict, uex, uex1, preconditioner)
+    f0 = nu*curl(curl(uex)) + sigma*diff(uex,t) + sigma*grad(uex1)
+    f1 = -div(sigma*grad(uex1)) - div(sigma*diff(uex,t))
 
-t_prev = T - d_t
+    bc_dict = {
+        "V": {
+            # Interpolate vector-valued exact solution on boundaries
+            (boundaries["bottom"], boundaries["top"], 
+            boundaries["front"], boundaries["right"],
+            boundaries["back"], boundaries["left"]): uex
+        },
+        "V1": {
+            # Interpolate scalar exact solution on all boundaries
+            (boundaries["bottom"], boundaries["top"], 
+            boundaries["front"], boundaries["right"],
+            boundaries["back"], boundaries["left"]): uex1
+        }
+    }
 
-uex_prev = exact(x, t_prev)
-uex_final = exact(x, T)
+    preconditioner = 'AMS'
 
-da_dt_exact = (uex_final - uex_prev) / d_t
-E_exact = -grad(uex1) - da_dt_exact
+    domain_tags = ct
 
-print("E field error", L2_norm(E - E_exact))
-print("B field error", L2_norm(B - curl(uex)))
-# %%
+    E,B = solver(comm, domain, ft, ct, degree, nu, sigma, t, d_t, num_steps, f0, f1, bc_dict, uex, uex1, preconditioner)
 
+    t_prev = T - d_t
 
-# %%
+    uex_prev = exact(x, t_prev)
+    uex_final = exact(x, T)
+
+    da_dt_exact = (uex_final - uex_prev) / d_t
+    E_exact = -grad(uex1) - da_dt_exact
+
+    print("E field error", L2_norm(E - E_exact))
+    print("B field error", L2_norm(B - curl(uex)))
