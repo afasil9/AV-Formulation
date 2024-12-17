075a8727a3a986ad0842897b3781d66b4b0e208d
Arshad Fasiludeen <arshadfasiludeen@dhcp-10-248-145-60.eduroam.wireless.private.cam.ac.uk>
2024-12-17 12:14:01 +0000

    Added symmetric solver and tests for symmetric case

--- tests.py
+++ tests.py
@@ -1,9 +1,12 @@
 from mpi4py import MPI
 from ufl import SpatialCoordinate, variable, as_vector, grad, curl, div, diff, sin, cos, pi, dot, cross, FacetNormal
 from solver_non_sym import solver
+from solver_sym import solver_sym
 from dolfinx import fem
 from utils import L2_norm, create_mesh_fenics, create_mesh_gmsh
 
+solver_type = "non symmetric"
+# solver_type = "symmetric"
 comm = MPI.COMM_WORLD
 degree = 1
 n = 4
@@ -16,23 +19,12 @@ d_t = (T - ti) / num_steps  # Time step size
 boundaries = {"bottom": 1, "top": 2, "front": 3, "right": 4, "back": 5, "left": 6}
 domain, ft, ct = create_mesh_fenics(comm, n, boundaries)
 
+# domain, ft, ct = create_mesh_gmsh(comm, n, boundaries)
+
 x = SpatialCoordinate(domain)
 t = variable(fem.Constant(domain, ti))
 nu_ = 1.0
 sigma_ = 1.0
-
-# def exact(x, t):
-#     return as_vector(
-#         (
-#             cos(pi * x[1]) * sin(pi * t),
-#             cos(pi * x[2]) * sin(pi * t),
-#             cos(pi * x[0]) * sin(pi * t),
-#         )
-#     )
-
-
-# def exact1(x):
-#     return sin(pi * x[0]) * sin(pi * x[1]) * sin(pi * x[2])
 
 def exact(x, t):
     return as_vector((
@@ -40,23 +32,34 @@ def exact(x, t):
         x[2]**2 + x[1] * t,
         x[0]**2 + x[2] * t))
 
-def exact1(x):
+def exact1_non(x):
     return (x[0]**2) + (x[1]**2) + (x[2]**2)
 
-uex = exact(x, t)
-uex1 = exact1(x)
+def exact1_time(x,t):
+    return (x[0]**2) * t + (x[1]**2) * t + (x[2]**2) * t
 
-f0 = nu_ * curl(curl(uex)) + sigma_ * diff(uex, t) + sigma_ * grad(uex1)
-f1 = -div(sigma_ * grad(uex1)) - div(sigma_ * diff(uex, t))
+uex = exact(x, t)
+norm = FacetNormal(domain)
+hn0 = cross(nu_ * curl(uex), norm)
+
+
+if solver_type == "non symmetric":
+    uex1 = exact1_non(x)
+    f0 = nu_ * curl(curl(uex)) + sigma_ * diff(uex, t) + sigma_ * grad(uex1)
+    f1 = -div(f0)
+    hn1 = -dot(norm, sigma_ * diff(uex, t) + sigma_ * grad(uex1))
+else:
+    uex1 = exact1_time(x,t)
+    f0 = nu_ * curl(curl(uex)) + sigma_ * diff(uex, t) + sigma_ * grad(diff(uex1, t))
+    f1 = -div(f0)
+    hn1 = -dot(norm, sigma_ * diff(uex, t) + sigma_ * grad(diff(uex1, t)))
+
 
 preconditioner = "AMS"
 
 domain_tags = ct
 facet_tags = ft
 
-norm = FacetNormal(domain)
-hn0 = cross(nu_ * curl(uex), norm)
-hn1 = -dot(norm, sigma_ * diff(uex, t) + sigma_ * grad(uex1))
 
 bc_dict = {
     "dirichlet": {
@@ -84,20 +87,37 @@ bc_dict = {
 }
 
 results = {
-    "postpro": True,
+    "postpro": False,
     "save_frequency": 10,
     "output_fields": ["A", "B", "V", "J", "E"]
 }
 
-E,B,J = solver(comm, domain, ft, ct, degree, nu_, sigma_, t, d_t, num_steps, f0, f1, bc_dict, hn0, hn1, uex, uex1, preconditioner, results)
+if solver_type == "non symmetric":
+    E,B,J = solver(comm, domain, ft, ct, degree, nu_, sigma_, t, d_t, num_steps, f0, f1, bc_dict, hn0, hn1, uex, uex1, preconditioner, results)
+    t_prev = T - d_t
 
-t_prev = T - d_t
+    uex_prev = exact(x, t_prev)
+    uex_final = exact(x, T)
 
-uex_prev = exact(x, t_prev)
-uex_final = exact(x, T)
+    da_dt_exact = (uex_final - uex_prev) / d_t
+    E_exact = -grad(uex1) - da_dt_exact
 
-da_dt_exact = (uex_final - uex_prev) / d_t
-E_exact = -grad(uex1) - da_dt_exact
+    print("E field error", L2_norm(E - E_exact))
+    print("B field error", L2_norm(B - curl(uex)))
+else:
+    E,B,J = solver_sym(comm, domain, ft, ct, degree, nu_, sigma_, t, d_t, num_steps, f0, f1, bc_dict, hn0, hn1, uex, uex1, preconditioner, results)
+    t_prev = T - d_t
 
-print("E field error", L2_norm(E - E_exact))
-print("B field error", L2_norm(B - curl(uex)))
+    uex_prev = exact(x, t_prev)
+    uex_final = exact(x, T)
+
+    da_dt_exact = (uex_final - uex_prev) / d_t
+
+    wex_prev = exact1_time(x, t_prev)
+    wex_final = exact1_time(x, T)
+
+    dw_dt_exact = (wex_final - wex_prev) / d_t
+    E_exact = -grad(dw_dt_exact) - da_dt_exact
+
+    print("E field error", L2_norm(E - E_exact))
+    print("B field error", L2_norm(B - curl(uex)))
